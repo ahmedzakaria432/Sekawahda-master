9f473619ede9c11e4324cb6b820542ed8c629f6e
Ahmed <ahmed.zakaria432@outlook.com>
2020-08-06 09:34:55 +0200

    a

--- SekkaWahda/Controllers/ProfileController.cs
+++ SekkaWahda/Controllers/ProfileController.cs
@@ -167,7 +167,7 @@ namespace SekkaWahda.Controllers
             catch (Exception ex)
             {
 
-                return Request.CreateErrorResponse(HttpStatusCode.OK, ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
             }
         }
 
